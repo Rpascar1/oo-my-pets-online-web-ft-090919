4c7088a5d406393053c108fd2ae75796a23e118f
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-24 13:11:27 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,11 +2,16 @@ class Cat
     attr_reader :name 
     attr_accessor:owner
    
+   
+   
    def initialize(name,owner)
      @name = name
      @owner= owner
+     @mood = "nervous"   
     end
-
+  
+    def mood
+      @mood 
   
 
 
