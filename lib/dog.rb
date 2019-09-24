1a3aad30e7b6167289494f09aad925e17dd63bc1
Richard Pascarelli <rpavesi84@gmail.com>
2019-09-24 13:13:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,32 @@
 class Dog
 
+    attr_reader :name 
+    attr_accessor:owner, :mood
+   
+   @@all = []
+   
+   def initialize(name,owner)
+     @name = name
+     @owner= owner
+     @mood = "nervous"  
+     @@all << self
+    end
   
+    def mood
+      @mood 
+    end
+    
+    def self.all
+      @@all
+    end
+    
+  
+
+
+
+
+
+end
 
 
 
